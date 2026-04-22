49c2fbac7024779fe310aae89f1ac5b1b605a623
@shivamkolekar8 <shivamkolekar8@gamil.com>
2026-04-22 22:50:45 +0530

    latest commit

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,22 @@
 package com.example.demo;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+
     @GetMapping("/")
-    public userController hello(){
-        return new userController("Shivam Kolekar.","Mobile & DevOps Engineer.");
+    public List<userController> hello(){
+
+        List<userController> list = new ArrayList<>();
+
+        list.add(new userController("Jon Deo.","Mobile Engineer."));
+        list.add(new userController("Recherson pari.","DevOps Engineer."));
+        list.add(new userController("Sam Desuja.","DevSecOps Engineer."));
+        return list;
     }
 }
